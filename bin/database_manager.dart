da5c624391f960f8e32936a338f08bd35a1e9865
Michel Forsch <michelforsch@gmail.com>
2022-05-01 13:01:01 -0300

    Feat(database): created new tables in database and add 'user_id' column
    * Create the tables 'address' to hold the address of each user, 'orders'
    to save all the orders that each user made and 'orders_foods' which has
    a many-to-many relationship to save all the foods that are in each order
    * Add 'users_id' to 'users' table and made that the PRIMARY KEY

--- bin/database_manager.dart
+++ bin/database_manager.dart
@@ -12,19 +12,47 @@ class Database {
 
   static Future<void> _configure() async {
     await _conn.query("CREATE DATABASE IF NOT EXISTS main_database;");
-
     await _conn.query("USE main_database;");
+
     await _conn.query("CREATE TABLE IF NOT EXISTS users ("
+        "user_id INT UNIQUE NOT NULL AUTO_INCREMENT, "
         "email VARCHAR(254) UNIQUE NOT NULL, "
         "username VARCHAR(255) UNIQUE NOT NULL, "
         "hashed_password VARCHAR(1024), "
         "salt VARCHAR(32), "
-        "PRIMARY KEY (email));");
+        "PRIMARY KEY (user_id));");
+    await _conn.query("CREATE TABLE IF NOT EXISTS address ("
+        "address_id INT UNIQUE NOT NULL AUTO_INCREMENT, "
+        "user_id INT UNIQUE NOT NULL, "
+        "rua VARCHAR(255) NOT NULL, "
+        "numero INT, "
+        "complemento VARCHAR(255), "
+        "bairro VARCHAR(255) NOT NULL, "
+        "cidade VARCHAR(255) NOT NULL, "
+        "cep VARCHAR(8) NOT NULL, "
+        "uf VARCHAR(2) NOT NULL, "
+        "PRIMARY KEY (address_id), "
+        "FOREIGN KEY (user_id) REFERENCES users (user_id));");
     await _conn.query("CREATE TABLE IF NOT EXISTS foods ("
-        "food_id INT UNIQUE NOT NULL, "
+        "food_id INT UNIQUE NOT NULL AUTO_INCREMENT, "
         "food_name VARCHAR(255) UNIQUE NOT NULL, "
         "food_price DOUBLE, "
         "PRIMARY KEY (food_id));");
+    await _conn.query("CREATE TABLE IF NOT EXISTS orders ("
+        "order_id INT UNIQUE NOT NULL AUTO_INCREMENT, "
+        "user_id INT UNIQUE NOT NULL, "
+        "order_date DATETIME, "
+        "order_total DOUBLE NOT NULL, "
+        "PRIMARY KEY (order_id), "
+        "FOREIGN KEY (user_id) REFERENCES users (user_id));");
+    // Many-to-many table
+    await _conn.query("CREATE TABLE IF NOT EXISTS orders_foods ("
+        "order_id INT NOT NULL, "
+        "food_id INT NOT NULL, "
+        "food_qty INT NOT NULL, "
+        "food_total DOUBLE NOT NULL, "
+        "FOREIGN KEY (order_id) REFERENCES orders (order_id), "
+        "FOREIGN KEY (food_id) REFERENCES foods (food_id));");
   }
 
   static Future<Results> queryFoods() async {
