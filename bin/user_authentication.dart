7c600363a74ce2b1e221670cb3adfb9e79aed6ad
Michel Forsch <michelforsch@gmail.com>
2022-05-03 21:50:52 -0300

    Fix: add missing return type to function authUserCredentials()

--- bin/user_authentication.dart
+++ bin/user_authentication.dart
@@ -10,7 +10,8 @@ class Auth {
   // TODO: implement password hashing (current solution only temporary)
   // TODO: implement JsonWebTokens to keep track of user session
   // static authenticateUserSession(JWT userSession...);
-  static authUserCredentials(String email, String password) async {
+  static Future<AuthResult> authUserCredentials(
+      String email, String password) async {
     // const String salt = Database.getUserSalt(email);
     // const String hashedPassword = Database.hashPassword(password, salt);
     // AuthResult result = Database.verifyCredentials(email, password);
@@ -18,6 +19,4 @@ class Auth {
     // if(result == AuthResult.authOk) generateJWT(...); return userSession;
     return result;
   }
-
-  
 }
